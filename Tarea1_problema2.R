7cc243286a286ac832e249c3d60aaf65d914dcc5
Sarha <sarha.cbg@gmail.com>
2021-02-21 21:21:03 -0600

    tareas

---
+++ Tarea1_problema2.R
@@ -0,0 +1,6 @@
+# Sarha Carolina ravo Gzz
+# 1907306
+germinación <- c(4, 1, 6, 2, 4, 2, 4, 2, 4, 6, 3, 5, 3, 2, 5, 4, 0, 5, 4, 2,
+                 4, 5, 3, 5, 3, 5, 4, 3, 6, 2)
+mean(germinación)
+sd(germinación)
